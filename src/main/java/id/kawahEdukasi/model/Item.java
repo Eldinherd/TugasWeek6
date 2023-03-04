899303ebbdaa5339175ad6d586c0ba69eefeeb05
Eldin Herdiana <eldinherdiana86@gmail.com>
2023-03-04 19:34:34 +0700

    TugasWeek6

---
+++ src/main/java/id/kawahEdukasi/model/Item.java
@@ -0,0 +1,43 @@
+package id.kawahEdukasi.model;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "item")
+public class Item extends PanacheEntityBase {
+
+    @Id
+    @SequenceGenerator(name = "itemSequence", sequenceName = "item_sequence",allocationSize = 1, initialValue = 1 )
+    @GeneratedValue(generator = "itemSequence", strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    public long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    public String name;
+
+    @Column(name = "count", nullable = false)
+    public Double count;
+
+    @Column(name = "price", nullable = false)
+    public Double price;
+
+    @Column(name = "type", nullable = false, length = 30)
+    public String type;
+
+    @Column(name = "description", columnDefinition = "text")
+    public String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, insertable=false)
+    public Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    public java.time.Instant updatedAt;
+}
