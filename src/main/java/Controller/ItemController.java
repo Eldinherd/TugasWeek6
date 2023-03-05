54da5d0dd603f20177deec10e4e299b66eec4a5d
Eldin Herdiana <eldinherdiana86@gmail.com>
2023-03-05 13:16:00 +0700

    Update Tugas Week6

--- src/main/java/Controller/ItemController.java
+++ src/main/java/Controller/ItemController.java
@@ -1,95 +1,57 @@
 package Controller;
 
 import id.kawahEdukasi.model.Item;
+import service.ItemService;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+
 
 @Path("/item")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ItemController {
-    @GET
-    public Response get(){
-        return Response.status(Response.Status.OK).entity(Item.findAll().list()).build();
-    }
+    @Inject
+    ItemService itemService;
 
     // Detail Item by id
+
+    @GET
+    public Response get(){
+        return itemService.get();
+    }
     @Path("/{id}")
     @GET
-    public Response get(@PathParam("id") Long id) {
-        Optional<Item> optionalItem = Item.findByIdOptional(id);
-        if (optionalItem.isEmpty()) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(Map.of("message", "Can't find Item"))
-                    .build();
-        }
-        Item item = optionalItem.get();
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("name", item.name);
-        response.put("count", item.count);
-        response.put("price", item.price);
-        response.put("type", item.type);
-        response.put("description", item.description);
-        return Response.status(Response.Status.OK).entity(response).build();
+    public Response get(@PathParam("id") Long id){
+        return itemService.get();
     }
+
+
 
     @POST
-    @Transactional
     public Response post(Map<String, Object> request){
-        Item item = new Item();
-        item.name = request.get("name").toString();
-        item.count = Double.valueOf(request.get("count").toString());
-        item.price = Double.valueOf(request.get("price").toString());
-        item.type = request.get("type").toString();
-        item.description = request.get("description").toString();
 
-        // Save to db
-        item.persist();
-
-        return Response.status(Response.Status.CREATED).entity(new HashMap<>()).build();
+        return itemService.post(request);
     }
+
 
     @PUT
     @Path("/{id}")
-    @Transactional
     public Response put(@PathParam("id") Long id, Map<String, Object> request){
-        Item item = Item.findById(id);
-        if(item == null){
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        item.name = request.get("name").toString();
-        item.count = Double.valueOf(request.get("count").toString());
-        item.price = Double.valueOf(request.get("price").toString());
-        item.type = request.get("type").toString();
-        item.description = request.get("description").toString();
 
-        // Save to db
-        item.persist();
-
-        return Response.status(Response.Status.CREATED).entity(new HashMap<>()).build();
+        return itemService.put(id, request);
     }
 
-    @DELETE
-    @Path("/{id}")
-    @Transactional
-    public Response delete(@PathParam("id") Long id){
-        Item item = Item.findById(id);
-        if(item == null){
-            return Response
-                    .status(Response.Status.BAD_REQUEST)
-                    .entity(Map.of("message", "ITEM_ALREADY_DELETED"))
-                    .build();
-        }
-        //Item.deleteById(id);
-        item.delete();
 
-        return Response.status(Response.Status.NO_CONTENT).entity(Map.of("id", item.id)).build();
+
+    @DELETE
+    @Path("/{id}") @Transactional
+    public Response delete(@PathParam("id") Long id){
+
+        return itemService.delete(id);
     }
 }
